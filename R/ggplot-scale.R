c3601f96beda074c0c70995b3fcc877c7049885d
Nash Delcamp <nash.delcamp@cognigencorp.com>
2021-05-24 16:06:34 -0400

    Update docs; pass R CMD CHECK

--- R/ggplot-scale.R
+++ R/ggplot-scale.R
@@ -1,96 +1,186 @@
-
+#' Discrete scale constructor
+#'
+#' @param n number of values to be mapped
+#' @param geom name of the geom; either 'point', 'line', or 'bar'
+#' @param style \code{list} of style elements
+#' @param grayscale \code{logical} indicating whether the plot should be grayscale
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' # TODO cleanup examples
+#'
+#' xydata <- read.csv(
+#'   file = './resources/xyplot_data.csv',
+#'   header = TRUE,
+#'   as.is = TRUE,
+#'   stringsAsFactors = FALSE
+#' )
+#' xydata$DOSE <- as.factor(xydata$DOSE)
+#'
+#' # scatter plot
+#' ggplot(data = xydata) +
+#'   aes(x = TIME, y = CONCENTRATION, colour = DOSE, fill = DOSE, shape = DOSE) +
+#'   geom_point() +
+#'   theme_cognigen() +
+#'   scale_discrete_cognigen(n = 10, geom = 'point')
+#'
+#' ggplot(data = xydata) +
+#'   aes(x = TIME, y = CONCENTRATION) +
+#'   geom_point() +
+#'   theme_cognigen() +
+#'   scale_discrete_cognigen(n = 10, geom = 'point')
+#'
+#' # Lineplot
+#' linedata <- subset(xydata, REP == 1)
+#' ggplot(data = linedata) +
+#'   aes(x = TIME, y = CONCENTRATION, group = DOSE) +
+#'   geom_line() +
+#'   theme_cognigen()
+#'
+#' # Barchart
+#' bardata <-  read.csv(
+#'   file = './resources/bar_data.csv',
+#'   header = TRUE,
+#'   as.is = TRUE,
+#'   stringsAsFactors = FALSE
+#' )
+#' bardata <- subset(bardata, GROUP > 0)
+#' bardata$GROUP <- as.factor(bardata$GROUP)
+#'
+#' ggplot(data = bardata) +
+#'   aes(x = STUDY, y = COUNT, fill = GROUP) +
+#'   geom_bar(stat = 'identity', position = 'stack', alpha = 1) +
+#'   theme_cognigen() +
+#'   scale_discrete_cognigen(n = 10, geom = 'bar')
+#'
+#' # Boxplot
+#' boxdata <- read.csv(
+#'   file = './resources/box_data.csv',
+#'   header = TRUE,
+#'   as.is = TRUE,
+#'   stringsAsFactors = FALSE
+#' )
+#' boxdata$GROUP <- as.factor(boxdata$GROUP)
+#' boxdata$CATEGORICAL <- as.factor(boxdata$CATEGORICAL)
+#'
+#' ggplot(data = boxdata) +
+#'   aes(x = GROUP, y = CONTINUOUS, colour = CATEGORICAL) +
+#'   geom_cognigenBoxplot(
+#'     notch = TRUE,
+#'     coef = 90,
+#'     fill = 'white',
+#'     #outlier.colour = NA,
+#'     outlier.jitter = TRUE,
+#'     outlier.size = 3,
+#'     position = position_dodge(width = 0.9),
+#'     na.rm = TRUE
+#'   ) +
+#'   theme_cognigen() +
+#'   scale_discrete_cognigen(10)
+#'
+#' # Histogram
+#' histdata <- read.csv(
+#'   file = './resources/hist_data.csv',
+#'   header = TRUE,
+#'   as.is = TRUE,
+#'   stringsAsFactors = FALSE
+#' )
+#' histdata$GROUP <- as.factor(histdata$GROUP)
+#' }
 scale_discrete_cognigen <- function(
-  n = 10, 
-  geom = 'point', 
+  n = 10,
+  geom = 'point',
   style = cognigen_style(),
   grayscale = FALSE
 ) {
-  
+
   # Input check
   if ( ! (is.integer(n) & n > 0) ){
     n <- 10
   }
-  
+
   if ( ! (geom %in% c('point', 'line', 'bar'))){
     stop('Invalid geom argument')
   }
-  
+
   default <- cognigen_style()
-  
+
   if ( !identical(get.structure(style), get.structure(default)) ){
     message('Invalid settings style. Reverting to default style.')
     style <- default
   }
-  
+
   if ( !is.logical(grayscale) ){
     stop('Invalid grayscale argument')
   }
   n <- n[1]
   geom <- geom[1]
   grayscale <- grayscale[1]
-  
+
   # Utility functions
   scales_col <- function(n, geom, style, grayscale){
-    
+
     grayscale <- ifelse(grayscale, 'grayscale', 'color')
-    
+
     geom <- switch(
       geom,
-      'point' = 'scatter', 
+      'point' = 'scatter',
       'line' = 'scatter',
       'bar' = 'bar'
     )
-    
+
     if ( n != 1 ){
       cols <- style[[geom]][[grayscale]]$col[-1]
     }
-    
+
     rep(
       cols,
       times = n%/%10 + ifelse(n%%10>0, 1, 0),
       length.out = n
     )
-    
+
   }
-  
+
   scales_fill <- function(n, geom, style, grayscale){
-    
+
     grayscale <- ifelse(grayscale, 'grayscale', 'color')
-    
+
     geom <- switch(
       geom,
-      'point' = 'scatter', 
+      'point' = 'scatter',
       'line' = 'scatter',
       'bar' = 'bar'
     )
-    
+
     if ( n != 1 ){
       fills <- style[[geom]][[grayscale]]$fill[-1]
     }
-    
+
     rep(
       fills,
       times = n%/%10 + ifelse(n%%10>0, 1, 0),
       length.out = n
     )
-    
+
   }
-  
+
   scales_pch <- function(n, geom, style, grayscale){
-    
+
     grayscale <- ifelse(grayscale, 'grayscale', 'color')
-    
+
     geom <- switch(
       geom,
-      'point' = 'scatter', 
+      'point' = 'scatter',
       'line' = 'scatter',
       'bar' = 'bar'
     )
-    
+
     if ( n != 1 ){
       pchs <- style[[geom]][[grayscale]]$pch[-1]
     }
-    
+
     if ( n > 10 ) {
       rep(
         pchs,
@@ -100,24 +190,24 @@ scale_discrete_cognigen <- function(
     } else {
       pchs[1:n]
     }
-    
+
   }
-  
+
   scales_lty <- function(n, geom, style, grayscale){
-    
+
     grayscale <- ifelse(grayscale, 'grayscale', 'color')
-    
+
     geom <- switch(
       geom,
-      'point' = 'scatter', 
+      'point' = 'scatter',
       'line' = 'scatter',
       'bar' = 'bar'
     )
-    
+
     if ( n != 1 ){
       ltys <- style[[geom]][[grayscale]]$lty[-1]
     }
-    
+
     if ( n > 10 ){
       rep(
         ltys,
@@ -127,102 +217,22 @@ scale_discrete_cognigen <- function(
     } else {
       ltys[1:n]
     }
-    
+
   }
-  
+
   # Function body
   if ( geom %in% c('point', 'line') ){
     list(
-      scale_colour_manual(values = scales_col(n, geom, style, grayscale)),
-      scale_fill_manual(values = scales_fill(n, geom, style, grayscale)),
-      scale_shape_manual(values = scales_pch(n, geom, style, grayscale)),
-      scale_linetype_manual(values = scales_lty(n, geom, style, grayscale))
+      ggplot2::scale_colour_manual(values = scales_col(n, geom, style, grayscale)),
+      ggplot2::scale_fill_manual(values = scales_fill(n, geom, style, grayscale)),
+      ggplot2::scale_shape_manual(values = scales_pch(n, geom, style, grayscale)),
+      ggplot2::scale_linetype_manual(values = scales_lty(n, geom, style, grayscale))
     )
   } else if ( geom %in% 'bar') {
     list(
-      scale_colour_manual(values = scales_col(n, geom, style, grayscale)),
-      scale_fill_manual(values = scales_col(n, geom, style, grayscale))
+      ggplot2::scale_colour_manual(values = scales_col(n, geom, style, grayscale)),
+      ggplot2::scale_fill_manual(values = scales_col(n, geom, style, grayscale))
     )
   }
-  
+
 }
-
-# scatter plot 
-require(ggplot2)
-
-xydata <- read.csv(
-  file = './resources/xyplot_data.csv',
-  header = TRUE,
-  as.is = TRUE,
-  stringsAsFactors = FALSE
-)
-xydata$DOSE <- as.factor(xydata$DOSE)
-
-ggplot(data = xydata) +
-  aes(x = TIME, y = CONCENTRATION, colour = DOSE, fill = DOSE, shape = DOSE) +
-  geom_point() +
-  theme_cognigen() +
-  scale_discrete_cognigen(n = 10, geom = 'point')
-
-ggplot(data = xydata) +
-  aes(x = TIME, y = CONCENTRATION) +
-  geom_point() +
-  theme_cognigen() +
-  scale_discrete_cognigen(n = 10, geom = 'point')
-
-# Lineplot
-linedata <- subset(xydata, REP == 1)
-ggplot(data = linedata) +
-  aes(x = TIME, y = CONCENTRATION, group = DOSE) +
-  geom_line() +
-  theme_cognigen()
-
-# Barchart
-bardata <-  read.csv(
-  file = './resources/bar_data.csv',
-  header = TRUE,
-  as.is = TRUE,
-  stringsAsFactors = FALSE
-)
-bardata <- subset(bardata, GROUP > 0)
-bardata$GROUP <- as.factor(bardata$GROUP)
-
-ggplot(data = bardata) +
-  aes(x = STUDY, y = COUNT, fill = GROUP) +
-  geom_bar(stat = 'identity', position = 'stack', alpha = 1) +
-  theme_cognigen() +
-  scale_discrete_cognigen(n = 10, geom = 'bar')
-
-# Boxplot
-boxdata <- read.csv(
-  file = './resources/box_data.csv',
-  header = TRUE,
-  as.is = TRUE,
-  stringsAsFactors = FALSE
-)
-boxdata$GROUP <- as.factor(boxdata$GROUP)
-boxdata$CATEGORICAL <- as.factor(boxdata$CATEGORICAL)
-
-ggplot(data = boxdata) +
-  aes(x = GROUP, y = CONTINUOUS, colour = CATEGORICAL) +
-  geom_cognigenBoxplot(
-    notch = TRUE,
-    coef = 90,
-    fill = 'white', 
-    #outlier.colour = NA,
-    outlier.jitter = TRUE,
-    outlier.size = 3,
-    position = position_dodge(width = 0.9),
-    na.rm = TRUE
-  ) +
-  theme_cognigen() +
-  scale_discrete_cognigen(10)
-  
-# Histogram
-histdata <- read.csv(
-  file = './resources/hist_data.csv',
-  header = TRUE,
-  as.is = TRUE,
-  stringsAsFactors = FALSE
-)
-histdata$GROUP <- as.factor(histdata$GROUP)
--- R/ggplot-theme.R
+++ R/ggplot-theme.R
@@ -1,86 +1,76 @@
-###############################################################################
-# Name: GitLab: R/shared-code/themes/ggplot-theme.R
-#
-# Copyright 2020, Cognigen Corporation.  The contents of this program are
-# confidential and cannot be used - in any form - for anything outside the
-# drug and specific project for which the file is provided.
-###############################################################################
-#
-# PURPOSE:
-# Create a ggplot2 theme; Consider different styles currently used across
-# the company.
-# This is a work in progress
-###############################################################################
-
-# all available components are described in the documentation
-?ggplot2::theme
-
-# Sections needed beyond theme --------------------------------------------
-
-# This file contains a {ggplot2} theme
-# The theme controls non-data components of the plot
-# We will also need to select color palettes for different aes elements
-# as well as shapes, line types, etc
-# see "ExploreLive styles functions" section in this file
-# The legend is not adjusted because most legends require specific adjustments
-
-
-# Cognigen theme ----------------------------------------------------------
-
-# Arguments passed to `...` further control the theme
-
+#' Cognigen ggplot2 themes
+#'
+#' @param ... additional theme elements to override defaults. See \link[ggplot2]{theme}.
+#'
+#' @export
+#'
+#' @importFrom ggplot2 `%+replace%`
+#'
+#' @examples
+#' \dontrun{
+#' p <- ggplot(mpg, aes(class, hwy)) +
+#'   geom_point()
+#'
+#' p + theme_cognigen()
+#'
+#' p + theme_cognigen_grid()
+#' }
 theme_cognigen <- function(...) {
-
-  if(!"ggplot2" %in% loadedNamespaces()) library(ggplot2)
 
   # font is set to "" which will default to "sans"
   font <- ""
 
   # start with a standard ggplot theme
-  theme_bw() %+replace%
+  ggplot2::theme_bw() %+replace%
 
-    theme(
+    ggplot2::theme(
 
       # grid elements
-      panel.grid.major = element_blank(),
-      panel.grid.minor = element_blank(),
+      panel.grid.major = ggplot2::element_blank(),
+      panel.grid.minor = ggplot2::element_blank(),
 
       # text elements
-      plot.title = element_text(
+      plot.title = ggplot2::element_text(
         family = font,
         size = 14,
         face = "bold",
         vjust = 1
       ),
 
-      plot.subtitle = element_text(
+      plot.subtitle = ggplot2::element_text(
         family = font,
         size = 11
       ),
 
-      plot.caption = element_text(
+      plot.caption = ggplot2::element_text(
         family = font,
         size = 9
       ),
 
-      axis.title = element_text(
+      axis.title = ggplot2::element_text(
         family = font,
         face = "bold",
         size = 11
       ),
 
-      axis.text = element_text(
+      axis.text = ggplot2::element_text(
         family = font,
         size = 10
-      ),
+      )
+    ) %+replace%
 
-      # user defined elements
-      ...
-
-    )
+    # user defined elements
+    ggplot2::theme(...)
 
 }
 
+
+#' @rdname theme_cognigen
+#'
+#' @param major.x,major.y,minor.x,minor.y \code{logical} indicating whether to
+#' draw major/minor grid lines on the respective axis
+#'
+#' @export
 theme_cognigen_grid <- function(
   major.x = TRUE,
   major.y = TRUE,
@@ -94,29 +84,32 @@ theme_cognigen_grid <- function(
   if ( !is.logical(minor.x) ) minor.x <- TRUE
   if ( !is.logical(minor.y) ) minor.y <- TRUE
 
-  theme_cognigen(...) %+replace%
+  theme_cognigen() %+replace%
 
-    theme(
+    ggplot2::theme(
       panel.grid.major.x = if (major.x[1]) {
-        element_line(colour = '#dddddd', size = 0.25)
+        ggplot2::element_line(colour = '#dddddd', size = 0.25)
       } else {
-        element_blank()
+        ggplot2::element_blank()
       },
       panel.grid.major.y = if (major.y[1]) {
-        element_line(colour = '#dddddd', size = 0.25)
+        ggplot2::element_line(colour = '#dddddd', size = 0.25)
       } else {
-        element_blank()
+        ggplot2::element_blank()
       },
       panel.grid.minor.x = if (minor.x[1]) {
-        element_line(colour = '#dddddd', size = 0.25)
+        ggplot2::element_line(colour = '#dddddd', size = 0.25)
       } else {
-        element_blank()
+        ggplot2::element_blank()
       },
       panel.grid.minor.y = if (minor.y[1]) {
-        element_line(colour = '#dddddd', size = 0.25)
+        ggplot2::element_line(colour = '#dddddd', size = 0.25)
       } else {
-        element_blank()
+        ggplot2::element_blank()
       }
-    )
+    ) %+replace%
+
+    # user defined elements
+    ggplot2::theme(...)
 
 }
