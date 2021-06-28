b61602f56e23ef39f149024e06d77bfde6770a92
sbihorel <sebastien.bihorel@cognigencorp.com>
2021-06-27 23:09:45 -0400

    Change default font size

--- R/geom-barcount.R
+++ R/geom-barcount.R
@@ -74,7 +74,7 @@ GeomBarcount <- ggplot2::ggproto(
   ggplot2::Geom,
   required_aes = c("x", "y"),
   default_aes = ggplot2::aes(
-    colour = "black", size = 3.88, angle = 0, hjust = 0.5,
+    colour = "black", size = 3, angle = 0, hjust = 0.5,
     vjust = 0.5 , alpha = NA, family = "", fontface = 1, lineheight = 1.2,
   ),
 
