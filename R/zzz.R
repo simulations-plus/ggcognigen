84cb574801db8753b70614013539d0132b6f5eeb
ndelcamp <nash.delcamp@cognigencorp.com>
2021-07-14 03:56:19 -0400

    change license to MIT

--- R/zzz.R
+++ R/zzz.R
@@ -9,35 +9,6 @@
       packageVersion('ggcognigen')
     )
   )
-  license <- system.file('LICENSE', package='ggcognigen')
-  if (length(options()$show_license) > 0){
-    show <- options()$show_license
-  } else {
-    show <- TRUE
-  }
-  if (length(license) > 0 & license != '' & show){
-    packageStartupMessage(
-      sprintf(
-        paste(
-          '********************************************************************************',
-          '',
-          'The use, copy, and re-distribution of the ggcognigen package are subject to the',
-          'terms specified in the license provided in:',
-          '  %s',
-          'Your use of the ggcognigen package is deemed acceptance of such license terms.',
-          'If you do not agree to such license terms, do not load or use the ggcognigen',
-          'package.',
-          '',
-          'Copyright 2020-%s Cognigen Corporation. All rights reserved.',
-          '',
-          '********************************************************************************',
-          sep = '\n'
-        ),
-        license,
-        format(Sys.Date(), '%Y')
-      )
-    )
-  }
 
   # Apply default Cognigen styling and theme for ggplot2
   set_default_style()
